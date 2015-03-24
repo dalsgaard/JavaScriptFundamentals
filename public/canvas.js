5e9bd03da66fe109c362056004547beebdbf891b
Kim Dalsgaard <kim@kimdalsgaard.com>
2015-03-24 11:26:06 +0100

    create example

--- public/canvas.js
+++ public/canvas.js
@@ -1,20 +1,28 @@
 
+var create = function(proto) {
+  var F = function() {};
+  F.prototype = proto;
+  return new F();
+};
+
 document.addEventListener('DOMContentLoaded', function() {
 
   var canvas = document.getElementById('tutorial');
   var ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = 'black';
-  ctx.moveTo(50, 50);
-  ctx.lineTo(100, 100);
-  ctx.stroke();
+  var c = create(ctx);
 
-  ctx.beginPath();
-  ctx.moveTo(150, 150);
-  ctx.strokeStyle = 'red';
-  ctx.lineTo(100, 400);
-  ctx.stroke();
+  c.fillStyle = "rgb(200,0,0)";
+  c.lineWidth = 5;
+  c.strokeStyle = 'black';
+  c.moveTo(50, 50);
+  c.lineTo(100, 100);
+  c.stroke();
+
+  c.beginPath();
+  c.moveTo(150, 150);
+  c.strokeStyle = 'red';
+  c.lineTo(100, 400);
+  c.stroke();
 
 });
